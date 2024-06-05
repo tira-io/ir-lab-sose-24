c7ff4dad33d7a89f2cd19efd4b3e0989e0d75293
Maik Fröbe <maik.froebe@uni-jena.de>
2024-06-05 11:30:23 +0200

    mf

--- ui/src/views/LandingPage.vue
+++ ui/src/views/LandingPage.vue
@@ -3,7 +3,7 @@
       <v-img class="mx-auto" height="300px" max-width="100%" width="300px" src="https://temir.org/img/index-cover-modified.png"/>
       <section>
         <h1 class="text-h3 text-sm-h3 py-4">
-          IR Lab SoSe 2024 at Augsburg/Jena/Köln/Leipzig
+          IR Lab SoSe 2024 at Augsburg/Jena/K&ouml;ln/Leipzig
         </h1>
         <p class="mx-auto py-4 tira-explanation">
           During the <a target="_blank" href="https://temir.org/teaching/information-retrieval-ss24/information-retrieval-ss24.html">IR lab in summer semester 2024</a> we did build search systems for the <a href="https://ir.webis.de/" target="_blank">IR Anthology</a> and the <a href="https://aclanthology.org/" target="_blank">ACL Anthology</a>. You can browse all the resulting systems and evaluations here.
@@ -16,7 +16,7 @@
             </v-btn>
           </v-col>
           <v-col cols="auto text-primary">
-            <v-btn href="https://www.tira.io/task-overview/ir-lab-sose-24" target="_blank"  min-width="228"  rel="noopener noreferrer" size="x-large" variant="outlined">
+            <v-btn href="https://www.tira.io/task-overview/ir-lab-sose-2024" target="_blank"  min-width="228"  rel="noopener noreferrer" size="x-large" variant="outlined">
               <v-icon size="large" start>
                 <v-img src="https://www.tira.io/public/tira/frontend-vuetify/assets-public/logo.svg" />
               </v-icon>
