966767f3de618fd2081bee829da9275a6086a9a4
Maik Fröbe <maik.froebe@uni-jena.de>
2024-06-30 13:32:36 +0200

    mf

--- ui/src/ir_datasets.ts
+++ ui/src/ir_datasets.ts
@@ -7,12 +7,15 @@ export default topics
 let data_access = {
 'databases': {
     'ir-lab-sose-2024/ir-acl-anthology-20240504-training': 'https://files.webis.de/data-in-progress/dataset-explorer-in-progress/ir-lab-sose-2024/ir-lab-sose-2024.db',
-    'ir-lab-sose-2024/ir-acl-anthology-topics-koeln-20240614-in-progress-test': 'https://files.webis.de/data-in-progress/dataset-explorer-in-progress/ir-lab-sose-2024/ir-lab-sose-2024.db'
-
+    'ir-lab-sose-2024/ir-acl-anthology-topics-koeln-20240614-in-progress-test': 'https://files.webis.de/data-in-progress/dataset-explorer-in-progress/ir-lab-sose-2024/ir-lab-sose-2024.db',
+    'ir-lab-sose-2024/ir-acl-anthology-topics-augsburg-20240525_0-test': 'https://files.webis.de/data-in-progress/dataset-explorer-in-progress/ir-lab-sose-2024/ir-lab-sose-2024.db',
+    'ir-lab-sose-2024/ir-acl-anthology-topics-leipzig-20240423-test': 'https://files.webis.de/data-in-progress/dataset-explorer-in-progress/ir-lab-sose-2024/ir-lab-sose-2024.db',
   },
   'documents': {
     'ir-lab-sose-2024/ir-acl-anthology-20240504-training': 'https://files.webis.de/data-in-progress/dataset-explorer-in-progress/ir-lab-sose-2024/inputs/documents.jsonl',
-    'ir-lab-sose-2024/ir-acl-anthology-topics-koeln-20240614-in-progress-test': 'https://files.webis.de/data-in-progress/dataset-explorer-in-progress/ir-lab-sose-2024/inputs/documents.jsonl'
+    'ir-lab-sose-2024/ir-acl-anthology-topics-koeln-20240614-in-progress-test': 'https://files.webis.de/data-in-progress/dataset-explorer-in-progress/ir-lab-sose-2024/inputs/documents.jsonl',
+    'ir-lab-sose-2024/ir-acl-anthology-topics-augsburg-20240525_0-test': 'https://files.webis.de/data-in-progress/dataset-explorer-in-progress/ir-lab-sose-2024/inputs/documents.jsonl',
+    'ir-lab-sose-2024/ir-acl-anthology-topics-leipzig-20240423-test': 'https://files.webis.de/data-in-progress/dataset-explorer-in-progress/ir-lab-sose-2024/inputs/documents.jsonl',
   }
 }
 
